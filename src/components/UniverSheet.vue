ebae33434b09b3bf247ba2e7aa32781508a4bc5e
白熱 <sonne@asaki.me>
2024-02-27 11:27:35 +0800

    update

--- src/components/UniverSheet.vue
+++ src/components/UniverSheet.vue
@@ -11,6 +11,7 @@ import "@univerjs/sheets-formula/lib/index.css";
 import { Univer } from "@univerjs/core";
 import { defaultTheme } from "@univerjs/design";
 import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
 import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
 import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
 import { UniverSheetsPlugin } from "@univerjs/sheets";
@@ -65,6 +66,7 @@ const init = (data = {}) => {
   univer.registerPlugin(UniverDocsPlugin, {
     hasScroll: false,
   });
+  univer.registerPlugin(UniverDocsUIPlugin);
 
   // sheet plugins
   univer.registerPlugin(UniverSheetsPlugin);
