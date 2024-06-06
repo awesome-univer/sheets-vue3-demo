6b278c87f910fa304f749411ac61a2ad04d79ba5
白熱 <sonne@asaki.me>
2024-06-06 11:17:02 +0800

    update

--- src/components/UniverSheet.vue
+++ src/components/UniverSheet.vue
@@ -14,6 +14,15 @@ import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
 import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
 import { UniverUIPlugin } from "@univerjs/ui";
 import { onBeforeUnmount, onMounted, ref } from "vue";
+
+/**
+ * 
+ * The ability to import locales from virtual modules and automatically import styles is provided by Univer Plugins. For more details, please refer to: https://univer.ai/guides/sheet/advanced/univer-plugins.
+ * If you encounter issues while using the plugin or have difficulty understanding how to use it, please disable Univer Plugins and manually import the language packs and styles.
+ * 
+ * 【从虚拟模块导入语言包】以及【自动导入样式】是由 Univer Plugins 提供的能力，详情参考：https://univer.ai/zh-CN/guides/sheet/advanced/univer-plugins
+ * 如果您在使用该插件的时候出现了问题，或者无法理解如何使用，请禁用 Univer Plugins，并手动导入语言包和样式
+ */ 
 import { zhCN, enUS } from 'univer:locales'
 
 const { data } = defineProps({
